7cb7b109dc9f232ff494ecd9e841eb15231ce928
Awesome User <61346229+saspav@users.noreply.github.com>
2022-06-17 22:33:09 +0300

    добавлен шаг 4.1.6

---
+++ step_4_1_6/test_main_page.py
@@ -0,0 +1,7 @@
+﻿from conftest import *
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
