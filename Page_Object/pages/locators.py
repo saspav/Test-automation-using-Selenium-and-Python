b3c6c31d62e7263efc739dd71aa62af41d9b5135
Awesome User <61346229+saspav@users.noreply.github.com>
2022-06-18 14:55:51 +0300

    пройден шаг 4.2.7

---
+++ Page_Object/pages/locators.py
@@ -0,0 +1,5 @@
+﻿from selenium.webdriver.common.by import By
+
+
+class MainPageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
--- Page_Object/test_main_page.py
+++ Page_Object/test_main_page.py
@@ -2,7 +2,7 @@
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     # инициализируем Page Object, передаем в конструктор экземпляр
     # драйвера и url адрес
     page = MainPage(browser, link)
@@ -13,7 +13,7 @@ def test_guest_can_go_to_login_page(browser):
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
