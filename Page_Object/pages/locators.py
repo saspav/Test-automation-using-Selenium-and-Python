294881e2133258f0428941aa094470561b6a37f4
Awesome User <61346229+saspav@users.noreply.github.com>
2022-06-18 17:31:00 +0300

    шаг 4.2.8 Реализация LoginPage

--- Page_Object/pages/locators.py
+++ Page_Object/pages/locators.py
@@ -3,3 +3,8 @@
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators:
+    LOGIN_FORM = (By.NAME, "login_submit")
+    REGISTER_FORM = (By.NAME, "registration_submit")
--- Page_Object/pages/main_page.py
+++ Page_Object/pages/main_page.py
@@ -1,5 +1,4 @@
 ﻿from .base_page import BasePage
-from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
 
 
